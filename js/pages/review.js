a0f1016959bdb16b31075598d2d1cd746d9ef853
hiheecho <110316147+hiheecho@users.noreply.github.com>
2022-11-25 10:01:30 +0900

    배포한 내용 업데이트
    배포한 내용 업데이트

--- js/pages/review.js
+++ js/pages/review.js
@@ -192,10 +192,9 @@ export const myReviewList = async () => {
     div.innerHTML = temp_html;
     
     if(isOwner == true) {
-      //temp_html.remove();
       commentList.appendChild(div);
     }
-  
+    
   });
 }
 
@@ -211,3 +210,62 @@ export const writeToggle = () => {
     writeBtn.value = "글쓰기";
   }
 };
+
+// export const showReview = async () => {
+//   const q = query(
+//     collection(dbService, "reviews"),
+//     orderBy("createdAt", "desc")
+//   );
+//   const querySnapshot = await getDocs(q);
+//   querySnapshot.forEach((doc) => {
+//     console.log(doc.data());
+//     const commentObj = {
+//       ...doc.data(),
+//       id: doc.id
+//     };
+//     cmtObjList.push(commentObj);
+//   });
+//   const commentList = document.getElementById("review-content");
+//   const currentUid = authService.currentUser.uid;
+ 
+//   commentList.innerHTML = "";
+//   cmtObjList.forEach((cmtObj) => {
+//     const isOwner = currentUid === cmtObj.creatorId;
+//     console.log(cmtObj.creatorId)
+//     console.log(isOwner)
+//     const temp_html =
+//     `<div class="my-comment-card">
+//     <div class="card-body my-card-body" style="background:url(${cmtObj.movieImage}) 20% 1% / cover no-repeat;">
+//         <div class="my-cmtAt">${new Date(cmtObj.createdAt).toString().slice(0, 15)}</div>
+//         <blockquote class="blockquote my-mb-0">
+//         <div class="my-content">
+//             <div class="my-nick-n"><img class="cmtImg" width="50px" height="50px" src="${cmtObj.profileImg}" alt="profileImg" />
+//                 <span>${cmtObj.nickname ?? "닉네임 없음"}</span>
+//             </div>
+//             <a href="#" class="fa fa-heart-o option-card"><span>18</span></a>
+//         </p>
+//         <p class="commentText my-title">${cmtObj.movieTitle}</p>
+//         <p class="commentText my-review-text">${cmtObj.review}</p>
+//         <p id="${cmtObj.id}" class="noDisplay">
+//         <input class="newtitleInput" type="text" maxlength="30" />
+//         <input class="newCmtInput" type="text" maxlength="30" />
+//         <button class="updateBtn" onclick="update_comment(event)">완료</button>
+//         <div id= "my-card-btn"class="${isOwner ? "updateBtns" : "noDisplay"}">
+//         <button onclick="onEditing(event)" class="editBtn btn btn-dark">수정</button>
+//         <button name="${cmtObj.id}" onclick="delete_comment(event)" class="deleteBtn btn btn-dark">삭제</button>
+//         </div> 
+//         </blockquote>
+//       </div>
+//     </div>
+//   </div>`;
+
+//   const div = document.createElement("div");
+//     div.classList.add("mycards");
+//     div.innerHTML = temp_html;
+    
+//     if(isOwner == true) {
+//       commentList.appendChild(div);
+//     }
+    
+//   });
+// }
